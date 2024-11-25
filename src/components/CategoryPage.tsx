0952fec2ee49c538e3c9fede2785a42a6b8e2ec5
jacques <gkounkou@gmail.com>
2024-11-24 20:06:43 -0800

    (chore) adding Categories

---
+++ src/components/CategoryPage.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+
+const CategoryPage: React.FC<{ theme: string; isFontBold: boolean }> = ({
+}) => {
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default CategoryPage;
+
